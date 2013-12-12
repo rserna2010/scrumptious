ca0da28398fca4dd648c42cfae79359da787eefb
Richie <rserna2010@gmail.com>
2013-12-12 09:41:21 -0800

    first commit

---
+++ js/app/controllers/bookmarks.js
@@ -0,0 +1,18 @@
+App.BookmarksController = Ember.ArrayController.extend({
+    pushSort: function(attribute) {
+        if (this.get("sortProperties.firstObject") == attribute) {
+            this.toggleProperty("sortAscending");
+        } else {
+            this.set("sortProperties", [ attribute ]);
+            this.set("sortAscending", true );
+    }
+   },
+
+    favorites: function(){
+        return this.filterProperty("favorite", true)
+    }.property("@each.favorite")
+
+    , regular: function(){
+        return this.filterProperty("favorite", false)
+    }.property("@each.favorite")
+});
